216feecc261c3cb3a7a34afe419af58d8109a19e
itomblack <itomblack@hotmail.co.uk>
2017-04-01 22:04:59 -0700

    js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,53 @@
-(function () { 'use strict';
-
-  console.log('test');
-
-
-
-
-
+$( document ).ready(function() {
+  
+  $(window).load(function() {  
+    $(".hide-all").hide();
+    $(".show-all").removeClass("show-all");
+  });
 
 
-}()); // end 'use strict'
+
+  (function($) {
+
+    /**
+     * Copyright 2012, Digital Fusion
+     * Licensed under the MIT license.
+     * http://teamdf.com/jquery-plugins/license/
+     *
+     * @author Sam Sehnert
+     * @desc A small plugin that checks whether elements are within
+     *     the user visible viewport of a web browser.
+     *     only accounts for vertical position, not horizontal.
+     */
+
+    $.fn.visible = function(partial) {
+      
+        var $t            = $(this),
+            $w            = $(window),
+            viewTop       = $w.scrollTop(),
+            viewBottom    = viewTop + $w.height(),
+            _top          = $t.offset().top,
+            _bottom       = _top + $t.height(),
+            compareTop    = partial === true ? _bottom : _top,
+            compareBottom = partial === true ? _top : _bottom;
+      
+      return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+
+    };
+      
+  })(jQuery);
+
+
+  $(window).scroll(function(event) {
+    
+    $(".img-wrap").each(function(i, el) {
+      var el = $(el);
+      if (el.visible(true)) {
+        el.addClass("come-in"); 
+      } 
+    });
+    
+  });
+
+
+});   //close document.ready
